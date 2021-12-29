79acc88fe1d5fbf8c22bf1ed04becd5854f550e4
snowmerak <snowmerak@outlook.com>
2021-12-30 01:39:32 +0900

    Update func.go

--- result/func.go
+++ result/func.go
@@ -1,14 +1,14 @@
 package result
 
-func Map[T any, R any](param Result[T], fn func(T) Result[R]) Result[R] {
+func Map[T any, R any](param *Result[T], fn func(T) *Result[R]) *Result[R] {
 	if param.Ok() {
 		return fn(param.Unwrap())
 	}
 	return Failed[R](param.Err())
 }
 
-func ToFunctor[T any, R any](fn func(T) R) func(Result[T]) Result[R] {
-	return func(param Result[T]) Result[R] {
+func ToFunctor[T any, R any](fn func(T) R) func(*Result[T]) *Result[R] {
+	return func(param *Result[T]) *Result[R] {
 		if param.Ok() {
 			return Success(fn(param.Unwrap()))
 		}
@@ -16,8 +16,8 @@ func ToFunctor[T any, R any](fn func(T) R) func(Result[T]) Result[R] {
 	}
 }
 
-func ToFunctorWithError[T any, R any](fn func(T) (R, error)) func(Result[T]) Result[R] {
-	return func(param Result[T]) Result[R] {
+func ToFunctorWithError[T any, R any](fn func(T) (R, error)) func(*Result[T]) *Result[R] {
+	return func(param *Result[T]) *Result[R] {
 		if param.Ok() {
 			value, err := fn(param.Unwrap())
 			if err != nil {
