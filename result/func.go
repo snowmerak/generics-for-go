7ec0048eef1963d56ccc0d18f99295f322c03479
snowmerak <snowmerak@outlook.com>
2021-12-29 21:45:38 +0900

    Create func.go

---
+++ result/func.go
@@ -0,0 +1,30 @@
+package result
+
+func Map[T any, R any](param Result[T], fn func(T) Result[R]) Result[R] {
+	if param.Ok() {
+		return fn(param.Unwrap())
+	}
+	return Failed[R](param.Err())
+}
+
+func ToFunctor[T any, R any](fn func(T) R) func(Result[T]) Result[R] {
+	return func(param Result[T]) Result[R] {
+		if param.Ok() {
+			return Success(fn(param.Unwrap()))
+		}
+		return Failed[R](param.Err())
+	}
+}
+
+func ToFunctorWithError[T any, R any](fn func(T) (R, error)) func(Result[T]) Result[R] {
+	return func(param Result[T]) Result[R] {
+		if param.Ok() {
+			value, err := fn(param.Unwrap())
+			if err != nil {
+				return Failed[R](err)
+			}
+			return Success(value)
+		}
+		return Failed[R](param.Err())
+	}
+}
