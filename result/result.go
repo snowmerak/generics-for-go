f3f3f5726d64c7578c8272641476e7c73a7c7f09
snowmerak <snowmerak@outlook.com>
2021-12-30 01:39:29 +0900

    Update result.go

--- result/result.go
+++ result/result.go
@@ -4,12 +4,46 @@ type Result[T any] struct {
 	value any
 }
 
-func (r Result[T]) Ok() bool {
+func Success[T any](data T) *Result[T] {
+	return &Result[T]{value: data}
+}
+
+func Ok[T any](data T) *Result[T] {
+	return &Result[T]{value: data}
+}
+
+func Failed[T any](err error) *Result[T] {
+	return &Result[T]{value: err}
+}
+
+func (r *Result[T]) Replace(fn func(T) *Result[T]) {
+	if r.Ok() {
+		n := fn(r.Unwrap())
+		if n.Ok() {
+			r.value = n.Unwrap()
+		} else {
+			r.value = n.Err()
+		}
+	}
+}
+
+func (r *Result[T]) ReplaceOr(fn func(T) *Result[T], defaultValue T) {
+	if r.Ok() {
+		n := fn(r.Unwrap())
+		if n.Ok() {
+			r.value = n.Unwrap()
+			return
+		}
+	}
+	r.value = defaultValue
+}
+
+func (r *Result[T]) Ok() bool {
 	_, ok := r.value.(error)
 	return !ok
 }
 
-func (r Result[T]) Err() error {
+func (r *Result[T]) Err() error {
 	err, ok := r.value.(error)
 	if !ok {
 		return nil
@@ -17,19 +51,29 @@ func (r Result[T]) Err() error {
 	return err
 }
 
-func (r Result[T]) Unwrap() T {
+func (r *Result[T]) AndThen(fn func(T) *Result[T]) *Result[T] {
+	if r.Ok() {
+		return fn(r.Unwrap())
+	}
+	return Failed[T](r.Err())
+}
+
+func (r *Result[T]) Unwrap() T {
 	value, _ := r.value.(T)
 	return value
 }
 
-func Success[T any](data T) Result[T] {
-	return Result[T]{value: data}
+func (r *Result[T]) UnwrapOr(defaultValue T) T {
+	value, ok := r.value.(T)
+	if !ok {
+		return defaultValue
+	}
+	return value
 }
 
-func Ok[T any](data T) Result[T] {
-	return Result[T]{value: data}
-}
-
-func Failed[T any](err error) Result[T] {
-	return Result[T]{value: err}
+func (r *Result[T]) UnwrapOrPanic(err error) T {
+	if r.Ok() {
+		return r.Unwrap()
+	}
+	panic(err)
 }
