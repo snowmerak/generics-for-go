81b25a01e77ffc9fc58a8a0c9aa88db267baa01d
snowmerak <snowmerak@outlook.com>
2021-12-30 13:22:58 +0900

    Update option.go

--- option/option.go
+++ option/option.go
@@ -4,20 +4,62 @@ type Option[T any] struct {
 	value any
 }
 
-func (o Option[T]) Ok() bool {
+func Some[T any](value T) *Option[T] {
+	return &Option[T]{value: value}
+}
+
+func None[T any]() *Option[T] {
+	return &Option[T]{value: nil}
+}
+
+func (o *Option[T]) Ok() bool {
 	_, ok := o.value.(T)
 	return ok
 }
 
-func (o Option[T]) Unwrap() T {
+func (o *Option[T]) Unwrap() T {
 	value, _ := o.value.(T)
 	return value
 }
 
-func Some[T any](value T) Option[T] {
-	return Option[T]{value: value}
+func (o *Option[T]) UnwrapOr(defaultValue T) T {
+	if o.Ok() {
+		return o.Unwrap()
+	}
+	return defaultValue
 }
 
-func None[T any]() Option[T] {
-	return Option[T]{value: nil}
+func (o *Option[T]) Replace(fn func(T) (T, error)) {
+	if o.Ok() {
+		value, err := fn(o.Unwrap())
+		if err != nil {
+			o.value = nil
+		} else {
+			o.value = value
+		}
+	}
+}
+
+func (o *Option[T]) ReplaceOr(fn func(T) (T, error), defaultValue T) {
+	if o.Ok() {
+		value, err := fn(o.Unwrap())
+		if err != nil {
+			o.value = defaultValue
+		} else {
+			o.value = value
+			return
+		}
+	}
+	o.value = defaultValue
+}
+
+func (o *Option[T]) AndThen(fn func(T) (T, error)) *Option[T] {
+	if o.Ok() {
+		value, err := fn(o.Unwrap())
+		if err != nil {
+			return None[T]()
+		}
+		return Some(value)
+	}
+	return None[T]()
 }
