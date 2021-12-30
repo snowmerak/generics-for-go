f0f8907d392617810ad61bf8e2251be816a27296
snowmerak <snowmerak@outlook.com>
2021-12-30 21:06:46 +0900

    Update chain.go

--- chain/chain.go
+++ chain/chain.go
@@ -13,35 +13,35 @@ type Chain[T, R any] struct {
 
 func From[T, R any](list ...any) *result.Result[*Chain[T, R]] {
 	if len(list) == 0 {
-		return result.Failed[*Chain[T, R]](errors.New("list is empty"))
+		return result.Err[*Chain[T, R]](errors.New("list is empty"))
 	}
 	if len(list) == 1 {
 		fun := reflect.TypeOf(list[0])
 		if fun.Kind() != reflect.Func {
-			return result.Failed[*Chain[T, R]](errors.New("list is not a function"))
+			return result.Err[*Chain[T, R]](errors.New("list is not a function"))
 		}
 		if fun.In(0) != reflect.TypeOf(new(T)).Elem() {
-			return result.Failed[*Chain[T, R]](errors.New("the function's parameter type is invalid"))
+			return result.Err[*Chain[T, R]](errors.New("the function's parameter type is invalid"))
 		}
 		if fun.Out(0) != reflect.TypeOf(new(R)).Elem() {
-			return result.Failed[*Chain[T, R]](errors.New("the function's return type is invalid"))
+			return result.Err[*Chain[T, R]](errors.New("the function's return type is invalid"))
 		}
 		return result.Success(&Chain[T, R]{list})
 	}
 	in := reflect.TypeOf(list[0]).In(0)
 	if in != reflect.TypeOf(new(T)).Elem() {
-		return result.Failed[*Chain[T, R]](errors.New("function's parameter type is invalid"))
+		return result.Err[*Chain[T, R]](errors.New("function's parameter type is invalid"))
 	}
 	out := reflect.TypeOf(list[0]).Out(0)
 	for i := 1; i < len(list); i++ {
 		in = reflect.TypeOf(list[i]).In(0)
 		if out != in {
-			return result.Failed[*Chain[T, R]](errors.New("function's parameter type is invalid"))
+			return result.Err[*Chain[T, R]](errors.New("function's parameter type is invalid"))
 		}
 		out = reflect.TypeOf(list[i]).Out(0)
 	}
 	if out != reflect.TypeOf(new(R)).Elem() {
-		return result.Failed[*Chain[T, R]](errors.New("function's return type is invalid"))
+		return result.Err[*Chain[T, R]](errors.New("function's return type is invalid"))
 	}
 	return result.Ok(&Chain[T, R]{list})
 }
