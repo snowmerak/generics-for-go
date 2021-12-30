0fdcec8bccaeb15d2f9b6d76a8710f23a28c168f
snowmerak <snowmerak@outlook.com>
2021-12-30 14:25:37 +0900

    Update func.go

--- result/func.go
+++ result/func.go
@@ -7,23 +7,14 @@ func Map[T any, R any](param *Result[T], fn func(T) *Result[R]) *Result[R] {
 	return Failed[R](param.Err())
 }
 
-func ToFunctor[T any, R any](fn func(T) R) func(*Result[T]) *Result[R] {
+func ToFunctor[T any, R any](fn func(T) (R, error)) func(*Result[T]) *Result[R] {
 	return func(param *Result[T]) *Result[R] {
 		if param.Ok() {
-			return Success(fn(param.Unwrap()))
-		}
-		return Failed[R](param.Err())
-	}
-}
-
-func ToFunctorWithError[T any, R any](fn func(T) (R, error)) func(*Result[T]) *Result[R] {
-	return func(param *Result[T]) *Result[R] {
-		if param.Ok() {
-			value, err := fn(param.Unwrap())
+			n, err := fn(param.Unwrap())
 			if err != nil {
 				return Failed[R](err)
 			}
-			return Success(value)
+			return Ok(n)
 		}
 		return Failed[R](param.Err())
 	}
