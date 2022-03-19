de81976e1cb09f4a7245bc54d6bbd6d7795f98ab
snowmerak <snowmerak@outlook.com>
2022-03-20 00:53:49 +0900

    add docs

--- syntax/cond/if.go
+++ syntax/cond/if.go
@@ -1,15 +1,19 @@
+// cond gives condition control flow
 package cond
 
+// ifCond is a pair of condition function and result function.
 type ifCond[T, R any] struct {
 	fun    func(T) bool
 	result func(T) R
 }
 
+// IfCond is a if condition control flow structure.
 type IfCond[T, R any] struct {
 	conds        []ifCond[T, R]
 	defaultValue func(T) R
 }
 
+// If returns a new IfCond with given condition.
 func If[T, R any](condition func(T) bool, result func(T) R) *IfCond[T, R] {
 	c := new(IfCond[T, R])
 	c.conds = []ifCond[T, R]{
@@ -21,6 +25,7 @@ func If[T, R any](condition func(T) bool, result func(T) R) *IfCond[T, R] {
 	return c
 }
 
+// ElseIf adds a new condition to the IfCond.
 func (c *IfCond[T, R]) ElseIf(condition func(T) bool, result func(T) R) *IfCond[T, R] {
 	c.conds = append(c.conds, ifCond[T, R]{
 		fun:    condition,
@@ -29,12 +34,14 @@ func (c *IfCond[T, R]) ElseIf(condition func(T) bool, result func(T) R) *IfCond[
 	return c
 }
 
+// Else adds a default result function to the IfCond.
 func (c *IfCond[T, R]) Else(defaultElse func(T) R) *IfCond[T, R] {
 	c.defaultValue = defaultElse
 	return c
 }
 
-func (c *IfCond[T, R]) Do(t T) (rs R) {
+// Run runs the IfCond with given value.
+func (c *IfCond[T, R]) Run(t T) (rs R) {
 	for _, cond := range c.conds {
 		ok := cond.fun(t)
 		if ok {
--- syntax/cond/switch.go
+++ syntax/cond/switch.go
@@ -1,10 +1,12 @@
 package cond
 
+// SwitchCond is a switch condition control flow structure.
 type SwitchCond[T comparable, R any] struct {
 	conds        map[T]func(T) R
 	defaultValue func(T) R
 }
 
+// Switch returns a new SwitchCond.
 func Switch[T comparable, R any]() *SwitchCond[T, R] {
 	return &SwitchCond[T, R]{
 		conds:        make(map[T]func(T) R),
@@ -12,17 +14,20 @@ func Switch[T comparable, R any]() *SwitchCond[T, R] {
 	}
 }
 
+// Case adds a new condition to the SwitchCond.
 func (s *SwitchCond[T, R]) Case(value T, result func(T) R) *SwitchCond[T, R] {
 	s.conds[value] = result
 	return s
 }
 
+// Default adds a default result function to the SwitchCond.
 func (s *SwitchCond[T, R]) Default(defaultElse func(T) R) *SwitchCond[T, R] {
 	s.defaultValue = defaultElse
 	return s
 }
 
-func (s *SwitchCond[T, R]) Do(t T) (rs R) {
+// Run runs the SwitchCond with given value.
+func (s *SwitchCond[T, R]) Run(t T) (rs R) {
 	if s.conds[t] != nil {
 		return s.conds[t](t)
 	}
