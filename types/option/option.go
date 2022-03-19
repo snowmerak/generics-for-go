44a274870c4af2bbaee61c19c4f9ea5fd03f5e92
snowmerak <snowmerak@outlook.com>
2022-03-20 00:03:45 +0900

    update docs

--- types/option/option.go
+++ types/option/option.go
@@ -1,10 +1,13 @@
+// option is a type that represents optional values.
 package option
 
+// Option is a container for a value of type T.
 type Option[T any] struct {
 	value  T
 	isSome bool
 }
 
+// Some returns a new Option with the given value.
 func Some[T any](value T) *Option[T] {
 	return &Option[T]{
 		value:  value,
@@ -12,16 +15,20 @@ func Some[T any](value T) *Option[T] {
 	}
 }
 
+// None returns a new Option with no value.
 func None[T any]() *Option[T] {
 	return &Option[T]{
 		isSome: false,
 	}
 }
 
+// IsSome returns true if the Option has a value.
 func (o *Option[T]) IsSome() bool {
 	return o.isSome
 }
 
+// Unwrap returns the value of the Option.
+// if Option does not have a value, returns zero value of T.
 func (o *Option[T]) Unwrap() (rs T) {
 	if o.isSome {
 		return o.value
@@ -29,6 +36,7 @@ func (o *Option[T]) Unwrap() (rs T) {
 	return rs
 }
 
+// UnwrapOr returns the value of the Option or the given value.
 func (o *Option[T]) UnwrapOr(defaultValue T) T {
 	if o == nil {
 		return defaultValue
@@ -39,6 +47,9 @@ func (o *Option[T]) UnwrapOr(defaultValue T) T {
 	return defaultValue
 }
 
+// Map transforms the Option by applying the given function to the value.
+// if Option does not have a value, do not applying.
+// if given function returns false, Option will be changed to None.
 func (o *Option[T]) Map(fn func(T) (T, bool)) {
 	if o.isSome {
 		value, b := fn(o.Unwrap())
@@ -50,6 +61,8 @@ func (o *Option[T]) Map(fn func(T) (T, bool)) {
 	}
 }
 
+// MapOr transforms the Option by applying the given function to the value.
+// if Option does not have a value or given function returns false, Option will be have defaultValue.
 func (o *Option[T]) MapOr(fn func(T) (T, bool), defaultValue T) {
 	if o.isSome {
 		value, b := fn(o.Unwrap())
@@ -63,6 +76,7 @@ func (o *Option[T]) MapOr(fn func(T) (T, bool), defaultValue T) {
 	o.value = defaultValue
 }
 
+// MapOrElse returns new Option by applying the given function to old Option.
 func (o *Option[T]) AndThen(fn func(T) (T, bool)) *Option[T] {
 	if o.isSome {
 		value, b := fn(o.Unwrap())
---
+++ types/readme.md
@@ -0,0 +1,19 @@
+# types
+
+## option
+
+`option` is optional struct for Type `T`.
+
+## result
+
+`result` is tuple type of type `T` and `error`.
+
+## pointer
+
+`pointer` is a atomic `unsafe.Pointer` wrapped generic.
+
+## tuple
+
+`tuple` presents multiple type set.
+
+more information is [here](tuple/readme.md).
