e98f441c1e4caeaea9345bc1def16921f69cd63e
snowmerak <snowmerak@outlook.com>
2022-03-20 00:16:09 +0900

    add docs

--- types/result/func.go
+++ types/result/func.go
@@ -1,16 +1,18 @@
 package result
 
+// Map returns new Result by applying the given function to the old result.
 func Map[T any, R any](param *Result[T], fn func(T) (R, error)) *Result[R] {
 	if param.isOk {
 		r, err := fn(param.Unwrap())
 		if err != nil {
 			return Err[R](err)
 		}
-		return Ok(r)
+		return OK(r)
 	}
 	return Err[R](param.Err())
 }
 
+// ToFunctor returns new function with Result by applying the given function to the old result.
 func ToFunctor[T any, R any](fn func(T) (R, error)) func(*Result[T]) *Result[R] {
 	return func(param *Result[T]) *Result[R] {
 		if param.isOk {
@@ -18,7 +20,7 @@ func ToFunctor[T any, R any](fn func(T) (R, error)) func(*Result[T]) *Result[R] 
 			if err != nil {
 				return Err[R](err)
 			}
-			return Ok(n)
+			return OK(n)
 		}
 		return Err[R](param.Err())
 	}
--- types/result/result.go
+++ types/result/result.go
@@ -1,12 +1,15 @@
+// result is a helper for handling errors.
 package result
 
+// Result is a container for values, T and error, bool.
 type Result[T any] struct {
 	value T
 	err   error
 	isOk  bool
 }
 
-func Ok[T any](data T) *Result[T] {
+// OK returns a new Result with the given value.
+func OK[T any](data T) *Result[T] {
 	return &Result[T]{
 		value: data,
 		err:   nil,
@@ -14,6 +17,7 @@ func Ok[T any](data T) *Result[T] {
 	}
 }
 
+// Err returns a new Result with the given error.
 func Err[T any](err error) *Result[T] {
 	return &Result[T]{
 		err:  err,
@@ -21,6 +25,7 @@ func Err[T any](err error) *Result[T] {
 	}
 }
 
+// Map transforms the Result by applying the given function to the value.
 func (r *Result[T]) Map(fn func(T) (T, error)) {
 	if r.isOk {
 		v, err := fn(r.Unwrap())
@@ -34,6 +39,8 @@ func (r *Result[T]) Map(fn func(T) (T, error)) {
 	}
 }
 
+// MapOr transforms the Result by applying the given function to the value.
+// if given function returns false, Result will be changed to OK(defaultValue).
 func (r *Result[T]) MapOr(fn func(T) (T, error), defaultValue T) {
 	if r.isOk {
 		v, err := fn(r.Unwrap())
@@ -45,29 +52,34 @@ func (r *Result[T]) MapOr(fn func(T) (T, error), defaultValue T) {
 	r.value = defaultValue
 }
 
-func (r *Result[T]) IsOk() bool {
+// IsOK returns true if the Result has a value.
+func (r *Result[T]) IsOK() bool {
 	return r.isOk
 }
 
+// Err returns the error of the Result.
 func (r *Result[T]) Err() error {
 	return r.err
 }
 
+// AndThen returns new Result by applying the given function to old Result.
 func (r *Result[T]) AndThen(fn func(T) (T, error)) *Result[T] {
 	if r.isOk {
 		v, err := fn(r.Unwrap())
 		if err != nil {
 			return Err[T](err)
 		}
-		return Ok(v)
+		return OK(v)
 	}
 	return Err[T](r.Err())
 }
 
+// Unwrap returns the value of the Result.
 func (r *Result[T]) Unwrap() T {
 	return r.value
 }
 
+// UnwrapOr returns the value of the Result or the given value.
 func (r *Result[T]) UnwrapOr(defaultValue T) T {
 	if r == nil {
 		return defaultValue
@@ -78,6 +90,8 @@ func (r *Result[T]) UnwrapOr(defaultValue T) T {
 	return r.value
 }
 
+// UnwrapOrPanic returns the value of the Result.
+// if Result does not have a value, panic with result's error.
 func (r *Result[T]) UnwrapOrPanic() T {
 	if r.isOk {
 		return r.Unwrap()
@@ -85,6 +99,8 @@ func (r *Result[T]) UnwrapOrPanic() T {
 	panic(r.err)
 }
 
+// UnwrapOrCustomPanic returns the value of the Result.
+// if Result does not have a value, panic with custom message.
 func (r *Result[T]) UnwrapOrCustomPanic(err error) T {
 	if r.isOk {
 		return r.Unwrap()
