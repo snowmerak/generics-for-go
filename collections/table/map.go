bf69bdf88dd3cb8899ea413429240fc1ae5fde0c
snowmerak <snowmerak@outlook.com>
2022-03-20 02:00:07 +0900

    add docs

--- collections/table/map.go
+++ collections/table/map.go
@@ -1,11 +1,16 @@
+// table is giving Table structure.
 package table
 
+// Table is a map[K]V.
 type Table[K comparable, V any] map[K]V
 
-func New[K comparable, V any]() map[K]V {
-	return make(map[K]V)
+// new creates a new Table.
+func New[K comparable, V any]() Table[K, V] {
+	return make(Table[K, V])
 }
 
+// FromSlices creates a new Table from the given slices.
+// key is the index of the slice.
 func FromSlices[K comparable, V any](ks []K, vs []V) map[K]V {
 	r := make(map[K]V)
 	for i := 0; i < len(ks) && i < len(vs); i++ {
--- collections/table/methods.go
+++ collections/table/methods.go
@@ -2,6 +2,7 @@ package table
 
 import "math/rand"
 
+// Clone returns a new Table with the same contents as this one.
 func (m Table[K, V]) Clone() map[K]V {
 	n := make(map[K]V)
 	for k, v := range m {
@@ -10,6 +11,7 @@ func (m Table[K, V]) Clone() map[K]V {
 	return n
 }
 
+// MapKey returns a new Table with the result of applying the function to each key.
 func (m Table[K, V]) MapKey(f func(K, V) K) map[K]V {
 	r := make(map[K]V)
 	for k, v := range m {
@@ -18,6 +20,7 @@ func (m Table[K, V]) MapKey(f func(K, V) K) map[K]V {
 	return r
 }
 
+// MapValue returns a new Table with the result of applying the function to each value.
 func (m Table[K, V]) MapValue(f func(K, V) V) map[K]V {
 	r := make(map[K]V)
 	for k, v := range m {
@@ -26,6 +29,7 @@ func (m Table[K, V]) MapValue(f func(K, V) V) map[K]V {
 	return r
 }
 
+// ReduceKey applies a function against an initial and each key in the Table.
 func (m Table[K, V]) ReduceKey(f func(K, V) K, initial K) K {
 	r := initial
 	for k, v := range m {
@@ -34,6 +38,7 @@ func (m Table[K, V]) ReduceKey(f func(K, V) K, initial K) K {
 	return r
 }
 
+// ReduceValue applies a function against an initial and each value in the Table.
 func (m Table[K, V]) ReduceVale(f func(K, V) V, initial V) V {
 	r := initial
 	for k, v := range m {
@@ -42,6 +47,7 @@ func (m Table[K, V]) ReduceVale(f func(K, V) V, initial V) V {
 	return r
 }
 
+// Any returns true if any element in the Table satisfies the predicate.
 func (m Table[K, V]) Any(f func(K, V) bool) bool {
 	for k, v := range m {
 		if f(k, v) {
@@ -51,6 +57,7 @@ func (m Table[K, V]) Any(f func(K, V) bool) bool {
 	return false
 }
 
+// All returns true if all elements in the Table satisfy the predicate.
 func (m Table[K, V]) All(f func(K, V) bool) bool {
 	for k, v := range m {
 		if !f(k, v) {
@@ -60,6 +67,7 @@ func (m Table[K, V]) All(f func(K, V) bool) bool {
 	return true
 }
 
+// Count returns the number of elements satisfy the predicate in the Table.
 func (m Table[K, V]) Count(f func(K, V) bool) int {
 	r := 0
 	for k, v := range m {
@@ -70,6 +78,7 @@ func (m Table[K, V]) Count(f func(K, V) bool) int {
 	return r
 }
 
+// Filter returns a new Table with all elements satisfy the predicate.
 func (m Table[K, V]) Filter(f func(K, V) bool) map[K]V {
 	r := make(map[K]V)
 	for k, v := range m {
@@ -80,7 +89,8 @@ func (m Table[K, V]) Filter(f func(K, V) bool) map[K]V {
 	return r
 }
 
-func (m Table[K, V]) Foreach(f func(K, V) (K, V)) map[K]V {
+// Map applies the function to each element in the Table.
+func (m Table[K, V]) Map(f func(K, V) (K, V)) map[K]V {
 	r := make(map[K]V)
 	for k, v := range m {
 		k, v = f(k, v)
@@ -89,6 +99,7 @@ func (m Table[K, V]) Foreach(f func(K, V) (K, V)) map[K]V {
 	return r
 }
 
+// Random returns a random element from the Table.
 func (m Table[K, V]) Random() (rk K, rv V) {
 	l := rand.Intn(len(m))
 	i := 0
