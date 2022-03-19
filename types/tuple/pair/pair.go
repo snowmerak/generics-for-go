c640b717895ee476b02510ae3c3e2234250d8be3
snowmerak <snowmerak@outlook.com>
2022-03-20 00:31:57 +0900

    add docs

--- types/tuple/pair/pair.go
+++ types/tuple/pair/pair.go
@@ -1,22 +1,28 @@
+// pair is a pair of values of the two types.
 package pair
 
+// pair is a container of type T and type R.
 type Pair[T, R any] struct {
 	a T
 	b R
 }
 
+// Of returns a new Pair with given values.
 func Of[T, R any](a T, b R) Pair[T, R] {
 	return Pair[T, R]{a, b}
 }
 
+// Get returns the values of the Pair.
 func (p Pair[T, R]) Get() (T, R) {
 	return p.a, p.b
 }
 
-func (p Pair[T, R]) GetA() T {
+// A returns the first value of the Pair.
+func (p Pair[T, R]) A() T {
 	return p.a
 }
 
-func (p Pair[T, R]) GetB() R {
+// B returns the second value of the Pair.
+func (p Pair[T, R]) B() R {
 	return p.b
 }
--- types/tuple/tripple/tri.go
+++ types/tuple/tripple/tri.go
@@ -1,27 +1,34 @@
+// tripple is a tripple of values with other types.
 package tripple
 
+// Tripple is a container of 3 values of the other types.
 type Tripple[A, B, C any] struct {
 	a A
 	b B
 	c C
 }
 
+// Of returns a new Tripple with given values.
 func Of[A, B, C any](a A, b B, c C) Tripple[A, B, C] {
 	return Tripple[A, B, C]{a, b, c}
 }
 
+// Get returns the values of the Tripple.
 func (t Tripple[A, B, C]) Get() (A, B, C) {
 	return t.a, t.b, t.c
 }
 
+// GetA returns the value of the first element.
 func (t Tripple[A, B, C]) GetA() A {
 	return t.a
 }
 
+// GetB returns the value of the second element.
 func (t Tripple[A, B, C]) GetB() B {
 	return t.b
 }
 
+// GetC returns the value of the third element.
 func (t Tripple[A, B, C]) GetC() C {
 	return t.c
 }
