905a6fa00c60bbe3b19e2cdcd401a012b38747ed
snowmerak <snowmerak@outlook.com>
2022-03-20 02:06:29 +0900

    add docs

--- collections/set/methods.go
+++ collections/set/methods.go
@@ -1,5 +1,6 @@
 package set
 
+// Clone returns a new Set with the same contents as this one.
 func (s Set[T]) Clone() Set[T] {
 	ns := New[T]()
 	for k := range s {
@@ -8,19 +9,23 @@ func (s Set[T]) Clone() Set[T] {
 	return ns
 }
 
+// Add adds the given value to the Set.
 func (s Set[T]) Add(v T) {
 	s[v] = struct{}{}
 }
 
+// Remove removes the given value from the Set.
 func (s Set[T]) Remove(v T) {
 	delete(s, v)
 }
 
+// Contains returns true if the given value is in the Set.
 func (s Set[T]) Contains(v T) bool {
 	_, ok := s[v]
 	return ok
 }
 
+// Union returns a new Set with the union of this Set and the given Set.
 func (s1 Set[T]) Union(s2 Set[T]) Set[T] {
 	union := Set[T]{}
 	for k := range s1 {
@@ -32,6 +37,7 @@ func (s1 Set[T]) Union(s2 Set[T]) Set[T] {
 	return union
 }
 
+// Intersect returns a new Set with the intersection of this Set and the given Set.
 func (s1 Set[T]) Intersect(s2 Set[T]) Set[T] {
 	intersection := Set[T]{}
 	for k := range s1 {
@@ -42,6 +48,7 @@ func (s1 Set[T]) Intersect(s2 Set[T]) Set[T] {
 	return intersection
 }
 
+// Subtract returns a new Set with the elements of this Set that are not in the given Set.
 func (s1 Set[T]) Subtract(s2 Set[T]) Set[T] {
 	subtract := Set[T]{}
 	for k := range s1 {
@@ -52,6 +59,7 @@ func (s1 Set[T]) Subtract(s2 Set[T]) Set[T] {
 	return subtract
 }
 
+// Equal returns true if this Set and the given Set are equal.
 func (s1 Set[T]) Equal(s2 Set[T]) bool {
 	if len(s1) != len(s2) {
 		return false
@@ -64,6 +72,7 @@ func (s1 Set[T]) Equal(s2 Set[T]) bool {
 	return true
 }
 
+// IsSubset returns true if this Set is a subset of the given Set.
 func (s1 Set[T]) IsSubset(s2 Set[T]) bool {
 	for k := range s1 {
 		if _, ok := s2[k]; !ok {
@@ -73,6 +82,7 @@ func (s1 Set[T]) IsSubset(s2 Set[T]) bool {
 	return true
 }
 
+// IsSuperset returns true if this Set is a superset of the given Set.
 func (s1 Set[T]) IsSuperset(s2 Set[T]) bool {
 	for k := range s2 {
 		if _, ok := s1[k]; !ok {
@@ -82,6 +92,7 @@ func (s1 Set[T]) IsSuperset(s2 Set[T]) bool {
 	return true
 }
 
+// IsDisjoint returns true if this Set and the given Set have no elements in common.
 func (s1 Set[T]) IsDisjoint(s2 Set[T]) bool {
 	for k := range s1 {
 		if _, ok := s2[k]; ok {
--- collections/set/set.go
+++ collections/set/set.go
@@ -1,11 +1,15 @@
+// set is giving Set structure.
 package set
 
+// Set is a map[T]struct{} with additional methods.
 type Set[T comparable] map[T]struct{}
 
+// New returns a new Set.
 func New[T comparable]() Set[T] {
 	return make(map[T]struct{})
 }
 
+// FromSlice returns a new Set from a slice.
 func FromSlice[T comparable](s []T) Set[T] {
 	set := Set[T]{}
 	for _, v := range s {
@@ -14,6 +18,12 @@ func FromSlice[T comparable](s []T) Set[T] {
 	return set
 }
 
+// Of returns a new Set with the given elements.
+func Of[T comparable](values ...T) Set[T] {
+	return FromSlice(values)
+}
+
+// FromMapKey returns a new Set with keys from a map.
 func FromMapKey[K comparable, V any](m map[K]V) Set[K] {
 	set := Set[K]{}
 	for k := range m {
@@ -22,6 +32,7 @@ func FromMapKey[K comparable, V any](m map[K]V) Set[K] {
 	return set
 }
 
+// FromMapValue returns a new Set with values from a map.
 func FromMapValue[K comparable, V comparable](m map[K]V) Set[V] {
 	set := Set[V]{}
 	for _, v := range m {
@@ -30,6 +41,7 @@ func FromMapValue[K comparable, V comparable](m map[K]V) Set[V] {
 	return set
 }
 
+// ToSlice returns a slice with the elements of the Set.
 func ToSlice[T comparable](s Set[T]) []T {
 	slice := make([]T, 0, len(s))
 	for k := range s {
