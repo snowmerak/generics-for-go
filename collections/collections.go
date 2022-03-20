b1c11da8aeeb5ea6587ac07d5da04c908b7820f0
snowmerak <snowmerak@outlook.com>
2022-03-20 17:20:50 +0900

    add docs

--- collections/collections.go
+++ collections/collections.go
@@ -1,6 +1,7 @@
 // collections package contains collection interfaces of generic types.
 package collections
 
+// Clonable is a interface for cloning.
 type Clonable[T any] interface {
 	Clone() T
 }
