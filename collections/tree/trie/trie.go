b107894b3033605b91bf21b932d51033ed712fc0
snowmerak <snowmerak@outlook.com>
2022-03-25 07:51:16 +0900

    Create trie.go

---
+++ collections/tree/trie/trie.go
@@ -0,0 +1,23 @@
+// trie
+package trie
+
+import "golang.org/x/exp/constraints"
+
+type node[T constraints.Ordered] struct {
+	value   T
+	less    *node[T]
+	equal   *node[T]
+	greater *node[T]
+}
+
+type Trie[T constraints.Ordered] struct {
+	root *node[T]
+}
+
+func New[T constraints.Ordered]() *Trie[T] {
+	return new(Trie[T])
+}
+
+func (t *Trie[T]) Insert(x ...T) error {
+
+}
