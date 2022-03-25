29dd9bb79012964c3eac2b9270fdf076e7a7be88
snowmerak <snowmerak@outlook.com>
2022-03-26 00:28:26 +0900

    Create operable.go

---
+++ interfaces/operable/operable.go
@@ -0,0 +1,23 @@
+package operable
+
+type Arithmetic[T any] interface {
+	Add(T) T
+	Sub(T) T
+	Mul(T) T
+	Div(T) T
+	Mod(T) T
+	Pow(T) T
+}
+
+type Bitwise[T any] interface {
+	Not(T) T
+	And(T) T
+	Or(T) T
+	Xor(T) T
+}
+
+type Shift[T any] interface {
+	LeftShift(T) T
+	LogicalShift(T) T
+	ArithmeticShift(T) T
+}
