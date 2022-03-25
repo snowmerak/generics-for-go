74763c5d6e158d6dc40f888e7666f4c5ce047b12
snowmerak <snowmerak@outlook.com>
2022-03-25 18:19:29 +0900

    remove clonable

--- collections/collections.go
+++
@@ -1,7 +0,0 @@
-// collections package contains collection interfaces of generic types.
-package collections
-
-// Clonable is a interface for cloning.
-type Clonable[T any] interface {
-	Clone() T
-}
