21666acf71356cbedffb885b12d7f868778039d8
snowmerak <snowmerak@outlook.com>
2022-03-25 18:32:54 +0900

    add docs

--- interfaces/copyable/copy.go
+++ interfaces/copyable/copy.go
@@ -1,9 +1,11 @@
 package copyable
 
+// Copyable copy to given object.
 type Copyable[T any] interface {
 	CopyTo(*T) error
 }
 
+// Clonable returns clone object.
 type Clonable[T any] interface {
 	Clone() T
 }
